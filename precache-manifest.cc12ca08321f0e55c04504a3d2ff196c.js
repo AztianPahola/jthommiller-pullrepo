60771424f7e01fcc41e1c2c1dce5b6930b52ff74
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-14 01:04:55 +0000

    Deploy jthommiller/CSC496-HackerNewsApp to github.com/jthommiller/CSC496-HackerNewsApp.git:gh-pages

---
+++ precache-manifest.cc12ca08321f0e55c04504a3d2ff196c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bad7e12cd5e3720a9048aac314aa40d",
+    "url": "/CSC496-HackerNewsApp/index.html"
+  },
+  {
+    "revision": "d9e716c384baa09b5277",
+    "url": "/CSC496-HackerNewsApp/static/css/main.ff3f25e4.chunk.css"
+  },
+  {
+    "revision": "e31886f0694ed13460b3",
+    "url": "/CSC496-HackerNewsApp/static/js/2.1d230a3f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CSC496-HackerNewsApp/static/js/2.1d230a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e716c384baa09b5277",
+    "url": "/CSC496-HackerNewsApp/static/js/main.f34807ab.chunk.js"
+  },
+  {
+    "revision": "79b31e5f75fb491f205a",
+    "url": "/CSC496-HackerNewsApp/static/js/runtime-main.2534e060.js"
+  }
+]);
